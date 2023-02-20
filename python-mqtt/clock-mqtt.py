f2b9ceceb4de02bace469d197a28f721043761c1
Emile Renaud <mimil2014@gmail.com>
2023-02-19 23:12:56 -0500

    send time python mqtt

---
+++ python-mqtt/clock-mqtt.py
@@ -0,0 +1,30 @@
+
+import time
+import datetime
+# try:
+import paho.mqtt.client as paho
+# except:
+#     print("Library paho not install")
+#     exit()
+
+broker = "192.168.2.101"
+port = 1883
+
+def on_publish(client,data,result):
+    print("data publish")
+    pass
+
+
+def main():
+    print("MQTT - Clock - Python")
+    client = paho.Client("clock1")
+    client.on_publish = on_publish
+    client.connect(broker,port)
+    loopVar = True
+    while(loopVar):
+        client.publish("clock/now",time.strftime("%H:%M:%S", time.localtime()))
+        time.sleep(1)
+
+
+if __name__== "__main__" :
+    main()
